7e7927eea11512017017ce29aaf879feada97c61
qiqiguai <qiqiguai@qq.com>
2019-09-03 19:19:26 +0800

    评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -28,9 +28,10 @@
         label="粉丝评论数">
       </el-table-column>
       <el-table-column label="操作">
+          <!-- 作用域插槽 slot-scope="变量" 变量  就是 row,column,$index.store的属性集合 obj.row -->
           <template slot-scope="obj">
               <el-button type='text' size="small" >修改评论</el-button>
-              <el-button type='text' size="small" :style='{color:obj.row.comment_status ? "#E6A23C" :"#409EFF"}'>{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+              <el-button @click="openOrClose(obj.row)" type='text' size="small" :style='{color:obj.row.comment_status ? "#E6A23C" :"#409EFF"}'>{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
           </template>
       </el-table-column>
     </el-table>
@@ -55,6 +56,22 @@ export default {
         params: { response_type: 'comment' }
       }).then(result => {
         this.list = result.data.results
+      })
+    },
+    // 打开或者关闭评论
+    openOrClose (row) {
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您是否要${mess}评论?`, '提示').then(() => {
+        // debugger
+        // 写调用接口
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id },
+          data: { allow_comment: !row.comment_status }
+        }).then((result) => {
+          this.getComments() // 成功之后 重新调用拉取数据的方法 => 前后台同步
+        })
       })
     }
   },
